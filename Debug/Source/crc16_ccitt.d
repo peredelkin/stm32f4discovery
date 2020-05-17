d9c6142c8f7ed9fd2039ab60998e8249ce2b8eb2
Ruslan <ep8kha@gmail.com>
2020-05-17 15:19:38 +0500

    no commit

--- Debug/Source/crc16_ccitt.d
+++
@@ -1,4 +0,0 @@
-Source/crc16_ccitt.o: ../Source/crc16_ccitt.c \
- /home/ruslan/eclipse-workspace/stm32f4discovery/Inclide/crc16_ccitt.h
-
-/home/ruslan/eclipse-workspace/stm32f4discovery/Inclide/crc16_ccitt.h:
--- Debug/Source/startup_stm32f407vgt6.d
+++
@@ -1,4 +0,0 @@
-Source/startup_stm32f407vgt6.o: ../Source/startup_stm32f407vgt6.c \
- ../Source/../Inclide/system_stm32f4xx.h
-
-../Source/../Inclide/system_stm32f4xx.h:
