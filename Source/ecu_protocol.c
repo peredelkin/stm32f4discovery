b7e626521604195018b790b426356b6aee84ce38
Ruslan Stoyanov <ep8kha@gmail.com>
2020-05-27 10:33:56 +0500

    оптимизация чтения и записи фрейма

--- Source/ecu_protocol.c
+++ Source/ecu_protocol.c
@@ -4,47 +4,43 @@ void ecu_read_frame_data(ecu_rw_t *ecu_r,volatile void **data) {
 	uint8_t type = ecu_r->frame.cmd_addr.cmd & ECU_DATA_TYPE_MASK;
 	uint16_t write_start = (ecu_r->frame.service_data.start / type);
 	uint16_t write_count = (ecu_r->frame.service_data.count / type) + 1;
-	uint16_t read_point = 0;
-	uint16_t write_point = 0;
+	uint16_t point = 0;
 	while (--write_count) {
 		switch (type) {
-		case ECU_DATA_TYPE_8: ((uint8_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + write_point] = *(uint8_t*)(&ecu_r->frame.data[read_point]);
+		case ECU_DATA_TYPE_8: ((uint8_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + point] = ((uint8_t*)(ecu_r->frame.data))[point];
 			break;
-		case ECU_DATA_TYPE_16: ((uint16_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + write_point] = *(uint16_t*)(&ecu_r->frame.data[read_point]);
+		case ECU_DATA_TYPE_16: ((uint16_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + point] = ((uint16_t*)(&ecu_r->frame.data))[point];
 			break;
-		case ECU_DATA_TYPE_32: ((uint32_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + write_point] = *(uint32_t*)(&ecu_r->frame.data[read_point]);
+		case ECU_DATA_TYPE_32: ((uint32_t*)(data[ecu_r->frame.cmd_addr.addr]))[write_start + point] = ((uint32_t*)(&ecu_r->frame.data))[point];
 			break;
 		default:
 			return;
 		};
-		read_point += type;
-		write_point++;
+		point++;
 	}
 }
 
-void ecu_write_frame_data(ecu_rw_t* ecu_w,volatile void **data,ecu_rw_t* ecu_r) {
-	ecu_w->frame.cmd_addr.cmd = ecu_r->frame.cmd_addr.cmd;
-	ecu_w->frame.cmd_addr.addr = ecu_r->frame.cmd_addr.addr;
-	ecu_w->frame.service_data.start = ecu_r->frame.service_data.start;
-	ecu_w->frame.service_data.count = ecu_r->frame.service_data.count;
-	uint8_t type = ecu_w->frame.cmd_addr.cmd & ECU_DATA_TYPE_MASK;
-	uint16_t read_start = (ecu_w->frame.service_data.start / type);
-	uint16_t read_count = (ecu_w->frame.service_data.count / type) + 1;
-	uint16_t write_point = 0;
-	uint16_t read_point = 0;
+void ecu_write_frame_data(ecu_rw_t* ecu_w,volatile void **data,uint8_t cmd,uint16_t addr,uint16_t start,uint8_t count) {
+	ecu_w->frame.cmd_addr.cmd = cmd;
+	ecu_w->frame.cmd_addr.addr = addr;
+	ecu_w->frame.service_data.start = start;
+	ecu_w->frame.service_data.count = count;
+	uint8_t data_type = ecu_w->frame.cmd_addr.cmd & ECU_DATA_TYPE_MASK;
+	uint16_t read_start = (ecu_w->frame.service_data.start / data_type);
+	uint16_t read_count = (ecu_w->frame.service_data.count / data_type) + 1;
+	uint16_t point = 0;
 	while (--read_count) {
-		switch (type) {
-		case ECU_DATA_TYPE_8: *(uint8_t*)(&ecu_w->frame.data[write_point]) = ((uint8_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + read_point];
+		switch (data_type) {
+		case ECU_DATA_TYPE_8: ((uint8_t*)(ecu_w->frame.data))[point] = ((uint8_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + point];
 			break;
-		case ECU_DATA_TYPE_16: *(uint16_t*)(&ecu_w->frame.data[write_point]) = ((uint16_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + read_point];
+		case ECU_DATA_TYPE_16: ((uint16_t*)(ecu_w->frame.data))[point] = ((uint16_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + point];
 			break;
-		case ECU_DATA_TYPE_32: *(uint32_t*)(&ecu_w->frame.data[write_point]) = ((uint32_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + read_point];
+		case ECU_DATA_TYPE_32: ((uint32_t*)(ecu_w->frame.data))[point] = ((uint32_t*)(data[ecu_w->frame.cmd_addr.addr]))[read_start + point];
 			break;
 		default:
 			return;
 		};
-		write_point += type;
-		read_point++;
+		point++;
 	}
 	ecu_w->count = ECU_CMD_ADDR_COUNT + ECU_SERVICE_DATA_COUNT + ecu_w->frame.service_data.count;
 	*(uint16_t*)(&ecu_w->frame.data[ecu_w->frame.service_data.count]) = crc16_ccitt((uint8_t*)(&ecu_w->frame),ecu_w->count);
@@ -83,7 +79,11 @@ void ecu_protocol_handler(ecu_protocol_t* protocol,usart_dma_t* usart_dma,volati
 				protocol->crc_read = *(uint16_t*)(&protocol->read.frame.data[0]);
 				protocol->crc_calc = crc16_ccitt((uint8_t*)(&protocol->read.frame),protocol->read.count_end - ECU_CRC_COUNT);
 				if(protocol->crc_read == protocol->crc_calc) {
-					ecu_write_frame_data(&protocol->write,directory,&protocol->read);
+					ecu_write_frame_data(&protocol->write,
+							directory,protocol->read.frame.cmd_addr.cmd,
+							protocol->read.frame.cmd_addr.addr,
+							protocol->read.frame.service_data.start,
+							protocol->read.frame.service_data.count);
 					usart_write(usart_dma,(uint8_t*)(&protocol->write.frame),protocol->write.count);
 				}
 			}
